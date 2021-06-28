adfd1febc0de3a00139d7d4d287dd550c55eabe9
ClaytonSiby <claytonsiby@gmail.com>
2021-06-28 23:54:20 +0200

    Initialize the Homepage component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import Homepage from './pages/Homepage';
 import './default.scss';
 
 const App = () => {
     return (
         <div>
             <Header />
+            <Homepage />
         </div>
     )
 }
---
+++ src/pages/Homepage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.scss';
+
+const Homepage = props => {
+    return (
+        <section>
+          <h1>Homepage</h1>
+        </section>
+    );
+};
+
+export default Homepage;
