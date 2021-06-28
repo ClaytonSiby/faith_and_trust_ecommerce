23b9b33c9161a99b7dcc1739aa6510162289c0f9
ClaytonSiby <claytonsiby@gmail.com>
2021-06-28 23:50:32 +0200

    Initialize the Header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
+import './default.scss';
 
-const App = () => (<div>Hello World</div>)
+const App = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.scss';
+
+const Header = props => {
+    return (
+        <header className="header">
+            <div className="wrap">
+                <div className="logo">
+                    <img src="" alt="faithNTrust" />
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
