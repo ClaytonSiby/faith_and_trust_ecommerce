e4af063b3802238236fbff78af0931282b6fd59e
ClaytonSiby <claytonsiby@gmail.com>
2021-06-29 10:04:14 +0200

    Add the Footer component

--- src/Layouts/HomepageLayout.js
+++ src/Layouts/HomepageLayout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const HomepageLayout = ({ children }) => (
   <div className="fullHeight">
     <Header />
     { children }
+    <Footer />
   </div>
 );
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="wrap">
+      &copy; 2021. All rights reserved.
+    </div>
+  </footer>
+);
+
+export default Footer;
