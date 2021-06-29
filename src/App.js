b628820cc74dfd9ca01836db2f0aced79ba3467b
ClaytonSiby <claytonsiby@gmail.com>
2021-06-29 09:50:52 +0200

    Introduce the Layout component and fix the Header component to include routing through links

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Header from './components/Header';
+// import Header from './components/Header';
+
+// layouts
+import MainLayout from './Layouts/MainLayout';
+import HomepageLayout from './Layouts/HomepageLayout';
+
+// pages
 import Homepage from './pages/Homepage';
 import Registration from './pages/Registration';
+
+// styles
 import './default.scss';
 
 const App = () => (
   <main className="App">
-    <Header />
-    <div className="main">
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route path="/registration" component={Registration} />
-      </Switch>
-
-    </div>
+    <Switch>
+      <Route
+        exact
+        path="/"
+        render={() => (
+          <HomepageLayout>
+            <Homepage />
+          </HomepageLayout>
+        )}
+      />
+      <Route
+        exact
+        path="/registration"
+        render={() => (
+          <MainLayout>
+            <Registration />
+          </MainLayout>
+        )}
+      />
+    </Switch>
   </main>
 );
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.scss';
 
 const Header = () => (
   <header className="header">
     <div className="wrap">
       <div className="logo">
-        <img src="" alt="faithNTrust" />
+        <Link to="/">
+          <img src="" alt="faithNTrust" />
+        </Link>
+      </div>
+
+      <div className="callToActions">
+        <ul>
+          <li>
+            <Link to="/registration">Register</Link>
+          </li>
+        </ul>
       </div>
     </div>
   </header>
