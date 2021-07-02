2897da920ee20e86902d9757755d8a2ef5d2a998
ClaytonSiby <claytonsiby@gmail.com>
2021-07-03 00:48:58 +0200

    Implement login with email and password functionality

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
 import Button from '../Forms/Button';
-import { signInWithGoogle } from '../../Firebase/utils';
+import FormInput from '../Forms/FromInput';
+import { signInWithGoogle, auth } from '../../Firebase/utils';
 import './styles.scss';
 
+const initialState = {
+  email: '',
+  password: '',
+};
+
 class SignIn extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ...initialState,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
   handleSubmit = async (e) => {
     e.preventDefault();
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+
+      this.setState({
+        ...initialState,
+      });
+    } catch (error) {
+      // console.log(error);
+    }
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+
+    this.setState({
+      [name]: value,
+    });
   }
 
   render() {
+    const { email, password } = this.state;
     return (
       <div className="signin">
         <div className="wrap">
@@ -16,6 +52,9 @@ class SignIn extends Component {
 
           <div className="formWrap">
             <form onSubmit={this.handleSubmit}>
+              <FormInput type="email" name="email" value={email} onChange={this.handleChange} placeholder="Email Address" />
+              <FormInput type="password" name="password" value={password} onChange={this.handleChange} placeholder="*******" />
+              <Button type="submit">Login</Button>
               <div className="socialSignin">
                 <div className="row">
                   <Button onClick={signInWithGoogle}>Google SignIn</Button>
