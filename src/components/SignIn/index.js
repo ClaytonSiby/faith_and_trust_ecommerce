68ab0826fa6240bca58997010899c692dc66ae86
ClaytonSiby <claytonsiby@gmail.com>
2021-07-02 21:30:02 +0200

    Add a Button component and its corresponding styles

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Button from '../Forms/Button';
 import './styles.scss';
 
 const SignIn = () => (
   <div className="signin">
     <div className="wrap">
       <h2>Login</h2>
+
+      <div className="formWrap">
+        <form>
+          <div className="socialSignin">
+            <div className="row">
+              <Button>
+                Google SignIn
+              </Button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 );
