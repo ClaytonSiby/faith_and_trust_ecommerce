6b7bd6efb2d7b371891257d6ce488660230f044f
ClaytonSiby <claytonsiby@gmail.com>
2021-07-12 17:09:39 +0200

    Update the rootSagas array to include the productsSagas in the store

--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -1,7 +1,8 @@
 import { all, call } from 'redux-saga/effects';
 import userSagas from './User/user.sagas';
+import productsSagas from './Products/products.sagas';
 
 // rootSaga generator function to combine all sagas for the application.
 export default function* rootSaga() {
-  yield all([call(userSagas)]);
+  yield all([call(userSagas), call(productsSagas)]);
 }
