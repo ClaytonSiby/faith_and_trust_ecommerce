d6da843aa48a907e976e9df5248d162419f74c3c
ClaytonSiby <claytonsiby@gmail.com>
2021-07-12 17:09:04 +0200

    Integrate/ add the productsReducer in the rootReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import userReducer from './User/user.reducer';
+import productsReducer from './Products/products.reducer';
 
 export default combineReducers({
   user: userReducer,
+  productsData: productsReducer,
 });
