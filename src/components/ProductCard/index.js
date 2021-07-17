20030a68ba7c8d26964684e10a9a5ca2c1be8e91
ClaytonSiby <claytonsiby@gmail.com>
2021-07-17 21:36:32 +0200

    Update the ProductCard component to render product organized information

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,8 +1,10 @@
+/* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable no-unused-vars */
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchProductStart } from '../../redux/Products/products.actions';
+import { fetchProductStart, setProduct } from '../../redux/Products/products.actions';
+import Button from '../Forms/Button';
 import './styles.scss';
 
 const mapState = (state) => ({
@@ -14,15 +16,41 @@ const ProductCard = () => {
   const { product } = useSelector(mapState);
   const { productID } = useParams();
 
-  const { productName } = product;
+  const { productThumbnail, productName, productPrice } = product;
 
   useEffect(() => {
     dispatch(fetchProductStart(productID));
+
+    return () => {
+      setProduct({});
+    };
   }, []);
 
+  const configAddToCartBtn = {
+    type: 'button',
+  };
+
   return (
-    <div>
-      <h1>{ productName }</h1>
+    <div className="productCard">
+      <div className="hero">
+        <img src={productThumbnail} alt="hero thumbnail" />
+      </div>
+      <div className="productDetails">
+        <ul>
+          <li><h1>{ productName }</h1></li>
+          <li>
+            <span>
+              $
+              {productPrice}
+            </span>
+          </li>
+          <li>
+            <div className="addToCart">
+              <Button {...configAddToCartBtn}>Add To Cart</Button>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
