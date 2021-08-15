0a8d5d1c13d3c9d1338afa7b9b74d83045df7285
Clayton Siby <claytonsiby@gmail.com>
2021-08-15 15:26:44 +0200

    Add the skeleton structure for the PaymentDetails component and basic styling

--- src/components/PaymentDetails/index.js
+++ src/components/PaymentDetails/index.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import { CountryDropdown } from 'react-country-region-selector';
 import FormInput from '../Forms/FormInput';
 import Button from '../Forms/Button';
 import './styles.scss';
@@ -12,7 +14,80 @@ const PaymentDetails = () => {
     <div className="paymentDetails">
       <form onSubmit={() => handleSubmit()}>
         <div className="group">
-          
+          <h2>Shipping Address</h2>
+
+          <FormInput
+            placeholder="Recipient Address"
+            type="text"
+          />
+
+          <FormInput
+            placeholder="line 1"
+            type="text"
+          />
+
+          <FormInput
+            placeholder="line 2"
+            type="text"
+          />
+
+          <FormInput
+            placeholder="City"
+            type="text"
+          />
+          <FormInput
+            placeholder="State"
+            type="text"
+          />
+          <FormInput
+            placeholder="Postal Code"
+            type="text"
+          />
+
+          <div className="formRow checkoutInput">
+            <CountryDropdown
+              valueType="short"
+            />
+          </div>
+        </div>
+        <div className="group">
+          <h2>Billing Address</h2>
+          <FormInput
+            placeholder="Name on Card"
+            type="text"
+          />
+
+          <FormInput
+            placeholder="line 1"
+            type="text"
+          />
+
+          <FormInput
+            placeholder="line 2"
+            type="text"
+          />
+
+          <FormInput
+            placeholder="City"
+            type="text"
+          />
+          <FormInput
+            placeholder="State"
+            type="text"
+          />
+          <FormInput
+            placeholder="Postal Code"
+            type="text"
+          />
+
+          <div className="formRow checkoutInput">
+            <CountryDropdown
+              valueType="short"
+            />
+          </div>
+        </div>
+        <div className="group">
+          <h2>Card Details</h2>
         </div>
       </form>
     </div>
