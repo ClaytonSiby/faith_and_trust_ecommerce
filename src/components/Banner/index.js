b9ff6c3e690e783c0c21571fcd26e6d7ebd5119c
ClaytonSiby <claytonsiby@gmail.com>
2023-08-15 10:18:44 +0200

    Add: more images and update the styling for the banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './styles.scss';
 
-const Banner = () => <div className="banner container-fluid">Banner Goes Here</div>;
+const Banner = () => (
+  <div className="banner">
+    <div>
+      <h2 className="text-light">
+        Place your orders online and get them delivered to you!
+      </h2>
+    </div>
+  </div>
+);
 
 export default Banner;
